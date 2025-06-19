5ed96b4263876fdc3d3244194057c9726e70fc4b
Rupam Das <rejisterjack@gmail.com>
2025-06-19 19:33:15 +0530

    bix: old deployment approach

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://chatapp-server-jeiz.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
 })
