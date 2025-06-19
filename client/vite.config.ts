67fcc49d8fa6b281a54e2018c54487f88ae93b1d
Rupam Das <rejisterjack@gmail.com>
2025-06-19 19:26:45 +0530

    fix: temp fix backend url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://chatapp-server-jeiz.onrender.com',
         changeOrigin: true,
       },
     },
